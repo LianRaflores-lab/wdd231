575e199372e55cbe1ce474d474635b4a308640ed
LianRaflores <lraflores@byupathway.edu>
2026-09-11 23:04:41 +0800

    week2

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,21 +1,32 @@
 async function getCompanies() {
-    const response = await fetch("../data/companies.json");
+    const response = await fetch("data/members.json");
     const datas = await response.json();
-    displayMembers(data.members);
 
     const cards = document.querySelector("#cards");
 
-    datas.array.forEach(data => {
-        const card = document.createElement("section");
+    function getMembershipLevel(membership) {
+        if (membership === 1) {
+            return "Member";
+        } else if (membership === 2) {
+            return "⭐⭐ Silver";
+        } else if (membership === 3) {
+            return "⭐⭐⭐ Gold";
+        }
+    }
+
+    datas.forEach(data => {
+        const card = document.createElement("div");
 
         card.innerHTML = `
-            <h2>${data.name}</h2>
-            <img class="info" src="images/${data.image}" alt="${data.name}">
-            <div class="info">
-                <p>Membership: <span id="${data.membership}><span>"
-                <p>Phone: ${data.phone}<p>
-                <p>Address: ${data.address}<p>
-                <p>Website: ${data.website}<p>
+            <div class="info-box">
+                <h2>${data.name}</h2>
+                <img class="info" src="images/${data.image}" alt="${data.name}">
+                <div class="info">
+                    <p>Membership: ${getMembershipLevel(data.membership)}</span></p>
+                    <p>Phone: ${data.phone}</p>
+                    <p>Address: ${data.address}</p>
+                    <p>Website: ${data.website}</p>
+                </div>
             </div>
         `;
 
