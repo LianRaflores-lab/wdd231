fe0920f2dd117a8919223b9b698303d3cff335e7
LianRaflores <lraflores@byupathway.edu>
2026-09-11 19:41:11 +0800

    week2

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,26 @@
-const navbutton = document.querySelector("#ham-btn");
-const navBar = document.querySelector('#nav-bar');
+async function getCompanies() {
+    const response = await fetch("../data/companies.json");
+    const datas = await response.json();
+    displayMembers(data.members);
 
-navbutton.addEventListener('click', () => {
-    navbutton.classList.toggle('show');
-    navBar.classList.toggle('show');
-})
+    const cards = document.querySelector("#cards");
+
+    datas.array.forEach(data => {
+        const card = document.createElement("section");
+
+        card.innerHTML = `
+            <h2>${data.name}</h2>
+            <img class="info" src="images/${data.image}" alt="${data.name}">
+            <div class="info">
+                <p>Membership: <span id="${data.membership}><span>"
+                <p>Phone: ${data.phone}<p>
+                <p>Address: ${data.address}<p>
+                <p>Website: ${data.website}<p>
+            </div>
+        `;
+
+        cards.appendChild(card)
+    });
+};
+
+getCompanies();
